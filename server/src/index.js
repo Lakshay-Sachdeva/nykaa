25ab2b66ef09970e3b27a3bbf764c85ca7c17a69
Lakshay Sachdeva <lakshay07sachdeva@gmail.com>
2022-01-19 13:33:21 +0530

    server initiated, user and product model done

---
+++ server/src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+//------------  Controllers ------------------- //
+
+//-------------- Models ---------------//
+
+const app = express();
+
+app.use(express.json());
+
+// app.use("" , controllers)
+
+module.exports = app;
