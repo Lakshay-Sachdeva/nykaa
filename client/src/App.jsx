37cf98be9dd1e35bc3bb234084efd022709a51bf
Swathi <swathi709357@gmail.com>
2022-01-21 16:51:37 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import SignupCard from './components/Signup'
+import Login from './components/login'
+const App = () => {
+ 
   return (
-    <div className="App">
-      
-    </div>
+    <Tabs variant='enclosed'>
+  <TabList mx='45%'>
+    <Tab color='pink.400'>Signup</Tab>
+    <Tab color='pink.400'>Login</Tab>
+  </TabList>
+  <TabPanels>
+    <TabPanel>
+     <SignupCard />
+    </TabPanel>
+    <TabPanel>
+      <Login />
+    </TabPanel>
+  </TabPanels>
+</Tabs>
+    
   );
-}
+};
 
 export default App;
