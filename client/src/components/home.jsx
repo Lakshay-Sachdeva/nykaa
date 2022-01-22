a4be4ff2091ed389a4f4308b13f2bb5df1899d55
Manthan Gaikwad <87421766+ManthanG369@users.noreply.github.com>
2022-01-22 19:49:00 +0530

    cart section

---
+++ client/src/components/home.jsx
@@ -0,0 +1,18 @@
+import { Cart } from "./payment/cart"
+import { Address } from "./payment/address"
+import { Stack, HStack, VStack, } from '@chakra-ui/react'
+import { Head } from "./payment/banner"
+import {Amount} from "./payment/amount"
+export const Home = () => {
+        return (
+                <>
+
+                        <Head/>
+                <HStack spacing='24px'>
+                        <Address />
+                        <Cart />
+                        <Amount />
+                </HStack>
+                </>
+        )
+}
