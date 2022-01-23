c924748e7f336d257527b7d0a26e79b416b1bf12
Alimul Hassan <alhassan069@gmail.com>
2022-01-23 14:50:10 +0530

    added productsGrid and Filter

--- client/src/Components/TopBrands.jsx
+++ client/src/Components/TopBrands.jsx
@@ -16,10 +16,11 @@ function TopBrands(data) {
 
     return (
          <div>
-            <Box mx="6%" >
+            <Box mx="6%" my="100px" >
                 <Grid templateColumns="repeat(2, 1fr)" gap={10} h='300px' w="98%">
                     {boxData.map((e) => (
                     <Box
+                        key={e.link}
                         // ratio={16/9}
                         backgroundImage={`url(${e.link})`}
                         // backgroundPosition="fit"
