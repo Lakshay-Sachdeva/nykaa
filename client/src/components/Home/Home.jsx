cd1da78d77b138b5d6090829acd88c20b27abb6e
Lakshay Sachdeva <lakshay07sachdeva@gmail.com>
2022-01-23 20:39:37 +0530

    deleted redundant files

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./NavBar";
+import { Carousels } from "../Carousels/carousel";
+import { SlideData } from "src/Asset/Images/sliderData1.jsx";
+
+export const Home = ({ show }) => {
+  return (
+    <>
+      <Navbar />
+
+      <Carousels show={SlideData} />
+    </>
+  );
+};
---
+++ client/src/components/Products/product.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "../App.css";
+import DetailsThumb from "./DetailsThumb";
+
+class Product extends React.Component {
+  state = {
+    products: [
+      {
+        _id: "1",
+        title: "Nykaa Rock The Line Kajal Eyeliner - Jet Black Set Of 2",
+        src: [
+          "https://i.ibb.co/6JkYhj1/kajal2.jpg",
+          "https://i.ibb.co/CbX75dL/kajal1.jpg",
+        ],
+
+        content: "inclusive of all taxes",
+        price: "MRP:₹398 | ₹338 |15% Off!",
+        count: 1,
+      },
+    ],
+    index: 0,
+  };
+
+  myRef = React.createRef();
+
+  handleTab = (index) => {
+    this.setState({ index: index });
+    const images = this.myRef.current.children;
+    for (let i = 0; i < images.length; i++) {
+      images[i].className = images[i].className.replace("active", "");
+    }
+    images[index].className = "active";
+  };
+
+  componentDidMount() {
+    const { index } = this.state;
+    this.myRef.current.children[index].className = "active";
+  }
+
+  render() {
+    const { products, index } = this.state;
+    return (
+      <div className="app">
+        {products.map((item) => (
+          <div className="details" key={item._id}>
+            <DetailsThumb
+              images={item.src}
+              tab={this.handleTab}
+              myRef={this.myRef}
+            />
+
+            <div className="big-img">
+              <img src={item.src[index]} alt="" />
+            </div>
+            <div className="line"></div>
+            <div className="box">
+              <div className="row">
+                <h1>{item.title}</h1>
+              </div>
+              <span>${item.price}</span>
+              <p>{item.content}</p>
+
+              <button className="cart">Add to cart</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Product;
